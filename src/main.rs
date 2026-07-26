4c86b310822b72150b811744324939a71b83a35a
quartascode <brenoposavac@proton.me>
2026-07-26 17:26:05 -0300

    basic grid

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+const HEIGHT: usize = 30;
+const WIDTH:  usize = 30;
+
+const ALIVE_CHAR: &str = "**";
+const DEAD_CHAR: &str = "--";
+
+fn index (x: usize, y: usize) -> usize {
+    return y * WIDTH + x;
+}
+
+fn draw(alive: bool) {
+    if alive {
+        print!("{}", ALIVE_CHAR);
+    } else {
+        print!("{}", DEAD_CHAR);
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cells = [false; HEIGHT * WIDTH];
+
+    cells[3 * WIDTH + 2] = true;
+    for i in 0..HEIGHT {
+        for j in 0..WIDTH {
+            draw(cells[index(j, i)]);
+        }
+        print!("\n");
+    }
 }
